ac4702202f7abdaae310b5c9abdd139d6e7ee86a
Brendson <brendson.net@gmail.com>
2022-06-27 22:13:55 -0300

    feat: created view for user login

---
+++ app/views/login.php
@@ -0,0 +1,30 @@
+<?php 
+  if (isset($_GET['erro'])) {
+    if ($_GET['erro'] == 1) {
+      $erro = "Acesso Negado!";
+    }
+  } else {
+    $erro = "";
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Controle de Tarefas</title>
+</head>
+<body>
+  <h3>Login</h3>
+  <form action="">
+    <input type="text" name="login"> <br>
+    <input type="password" name="senha"> <br>
+    <button>Enviar</button>
+    <span><?php echo $erro; ?></span>
+  </form>
+
+  <a href="./cadastro.php">Cadastre-se</a>
+</body>
+</html>
